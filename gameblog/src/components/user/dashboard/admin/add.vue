c0e240a2af4a550391bff26d350e5e52858abd38
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-05-13 07:43:48 +0300

    added dashboard content and started add article comp

---
+++ gameblog/src/components/user/dashboard/admin/add.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <h1>Add article</h1>
+    <hr/>
+
+    <Form class="mb-5" @submit="onSubmit">
+        <div class="mb-4">
+            <!-- Name of the game -->
+                <Field name="game" v-slot="{field,errors,errorMessage}">
+                    <input 
+                        type="password"
+                        placeholder="Name of the game"
+                        v-bind="field"
+                        class="form-control"
+                        :class="{'is-invalid':errors.length !== 0 }"
+                    />
+
+                    <div class="input_alert"v-if="errors.length !==0">
+                        {{ errorMessage }}
+                    </div>
+                </Field>
+            </div>
+    </Form>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import { Field, Form } from 'vee-validate';
+
+    function onSubmit(values,{ resetForm }) {
+        
+    }
+
+</script>
+
