3b21cfc821f59c70740aee4d29c02a6f0625c1ec
AntalNorbert23 <anorbertcsaba@yahoo.com>
2024-05-28 14:59:11 +0300

    added more editing possibility to article editor component

--- gameblog/src/utils/editor.vue
+++ gameblog/src/utils/editor.vue
@@ -29,7 +29,50 @@
                 @click="editor.chain().focus().toggleBulletList().run()" 
                 :class="{ 'is-active': editor.isActive('bulletList') }"
             >
-                bullet list
+                <i class="fa fa-list-ul"></i>
+            </span>
+            <span 
+                @click="editor.chain().focus().toggleUnderline().run()" 
+                :class="{ 'is-active': editor.isActive('underline') }" 
+            >
+                <u>U</u>
+            </span>
+            <span 
+                @click="editor.chain().focus().toggleSuperscript().run()" 
+                :class="{ 'is-active': editor.isActive('superscript') }" 
+            >
+                X<sup>2</sup>
+            </span>
+            <span 
+                @click="editor.chain().focus().toggleSubscript().run()" 
+                :class="{ 'is-active': editor.isActive('subscript') }" 
+            >
+                X<sub>2</sub>
+            </span>
+            <span 
+                @click="editor.chain().focus().setTextAlign('left').run()" 
+                :class="{ 'is-active': editor.isActive({textAlign:'left'}) }"
+                class="fa fa-align-left" 
+            >
+            </span>
+            <span 
+                @click="editor.chain().focus().setTextAlign('center').run()" 
+                :class="{ 'is-active': editor.isActive({textAlign:'center'}) }"
+                class="fa fa-align-center"
+            >
+            <i ></i>
+            </span>
+            <span 
+                @click="editor.chain().focus().setTextAlign('right').run()" 
+                :class="{ 'is-active': editor.isActive({textAlign:'right'}) }" 
+                class="fa fa-align-right"
+            >
+            </span>
+            <span 
+                @click="editor.chain().focus().toggleHighlight({ color: '#ffc078' }).run()" 
+                :class="{ 'is-active': editor.isActive('highlight', { color: '#ffc078' }) }"
+                class="fa fa-marker"
+            >
             </span>
 
         </div>
@@ -40,15 +83,27 @@
 
 <script setup>
     import { watch } from 'vue';
+    import Highlight from '@tiptap/extension-highlight'
+    import TextAlign from '@tiptap/extension-text-align'
+    import Superscript from '@tiptap/extension-superscript'
+    import Subscript from '@tiptap/extension-subscript'
+    import Underline from '@tiptap/extension-underline'
     import { Editor, EditorContent } from '@tiptap/vue-3'
     import StarterKit from '@tiptap/starter-kit'
 
     const emit=defineEmits(['update']);
     const props=defineProps(['content']);
     const editor = new Editor({
-        conten:'',
+        content:'',
         extensions:[
-            StarterKit
+            StarterKit,
+            Underline,
+            Superscript,
+            Subscript,
+            TextAlign.configure({
+                types:['heading','paragraph']
+            }),
+            Highlight.configure({ multicolor: true }),
         ],
         onUpdate:()=>{
             emit('update',editor.getHTML())
